4af95ffaa05921450cc080accf745691b4585278
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2023-02-25 08:45:05 -0300

    Começando os estudos em JS

---
+++ Arquitetura/Aula 01 - Arquitetura/Script.js
@@ -0,0 +1,43 @@
+var idade = 18
+console.log("Oi, "+ nome)
+if(idade>=18){
+    var nome = "Joao"// hoist (joga a variavel p fora do if)
+    console.log(nome + ", você pode dirigir")
+}
+console.log("Até mais, "+ nome)
+
+
+// var linguagem = "JavaScript"
+// console.log("Aprendendo"+ " " + linguagem)
+// linguagem = "Java"
+// console.log("Aprendendo"+ " " + linguagem)
+
+// var endereco = "Rua C"
+// console.log(endereco)
+// endereco = "Rua B"
+// console.log(endereco)
+// console.log(typeof(endereco))
+
+
+//ECMAScript
+//const, let, var 
+// const nome1 = "Ana"
+// const nome2 = "Pedro"
+// const nome3 = "José"
+
+// console.log(typeof(nome1))
+// console.log(typeof(nome2))
+// console.log(typeof(nome3))
+//REPL : Read, Evaluate, Print, Loop
+
+// let a = 2 
+// let b = 'abc'
+// console.log(typeof(b))
+// b = 12 
+// console.log(typeof(b))
+// b = true
+// console.log(typeof(b))
+
+
+
+
