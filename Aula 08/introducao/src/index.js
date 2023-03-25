8d78da3ba0b75e584b222987a64417e7f0ad9cc4
Marins14 <matheus.bernardello@live.com>
2023-03-25 12:53:17 -0300

    Começo de REACT

---
+++ Aula 08/introducao/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX: JavaScript Extension (XML)
+const App = () =>{
+    return(
+        <div>
+            Meu primeiro componente React
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
