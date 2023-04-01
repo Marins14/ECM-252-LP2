2e4f9c617704ccde43ce782564ea083fc18a7f40
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2023-04-01 10:54:47 -0300

    Fim da apostila 3

--- Aula 08/introducao/src/index.js
+++ Aula 08/introducao/src/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import './styles.css'
 // JSX: JavaScript Extension (XML)
 const App = () =>{
+    const EstiloBotao = {marginTop:12, paddingBottom:8,paddingTop:8, backgroundColor: "blueviolet",color: "white",border:"none", width: "100%", borderRadius: 8}
+
+    const TextoRotulo = 'Nome'
+    const obterTextoBotao = ()=> "Enviar" 
+    const onClique = () => alert('Click')
     return(
-        <div>
-            Meu primeiro componente React
+        <div style={{
+        margin: "auto",
+        width: 768,
+        backgroundColor: "#EEE",
+        padding: 12,
+        borderRadius: 8}}>
+            <label htmlFor="nome" style={{
+                dispay: "block",
+                marginBottom: 4,
+                }}>
+                    {TextoRotulo}
+            </label>
+            <input 
+                type="text" 
+                id="nome" 
+                style={{paddingTop: 8,paddingBottom: 8, borderStyle: "hidden", outline: "none", width: "100%", borderRadius: 8,boxSizing: 'border-box'}}/>
+            <button 
+            onClick = {onClique}
+            style={EstiloBotao}>
+                {obterTextoBotao()}
+            </button>
+
+            
         </div>
     )
 }
