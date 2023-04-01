f214e1e617f03e9dd5f9b54e22c8ae45a096fcad
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2023-04-01 11:32:33 -0300

    Começo de props

---
+++ Aula 09/componentes-e-props/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = ()=>(
+    <div>
+        Um componente
+    </div>
+)
+
+ReactDOM.render(
+    <App />, 
+        document.querySelector('root'))
