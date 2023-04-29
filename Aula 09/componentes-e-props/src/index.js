65a9b8be63319bd5da3f04f7ee42c3588d9c9af4
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2023-04-29 10:57:48 -0300

    Fim da apostila 04

--- Aula 09/componentes-e-props/src/index.js
+++ Aula 09/componentes-e-props/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import Pedido from './pedido.js'
+import Cartao from './cartao.js'
+import Feedback from './feedback.js'
 
-const App = ()=>(
-    <div className='container border rounded mt-2'>
+const App = ()=>{
+    const textoOK = 'Já chegou'
+    const textoNOK = 'Não chegou ainda'
+    const funcaoOK = ()=> alert("Agradecemos a confirmacao")
+    const funcaoNOK = ()=> alert("Verficaremos o ocorrido")
+    const componenteFeedback = <Feedback textoOK = {textoOK} textoNOK={textoNOK} funcaoOK = {funcaoOK} funcaoNOK={funcaoNOK}></Feedback>
+    return <div className='container border rounded mt-2'>
         <div ClassName = 'row border-bottom my-2'>
             <div className = 'col-12'>
                 <h1 className = 'display-5 text-center'>Seus pedidos</h1>
@@ -13,26 +20,35 @@ const App = ()=>(
         </div>
         <div className = 'row'>
             <div className = 'col-12 col-lg-6 col-xxl-4 my-2'>
-            <Pedido data="22/04/2023" 
-            icone="fas fa-ghost fa 2x"
-            titulo = "SSD"
-            descricao = "SSD Kinsgton A400 - SATA"  />
-            </div>
+                <Cartao cabecalho = "22/04/2021">
+                <Pedido
+                    icone="fas fa-ghost fa 2x"
+                    titulo = "SSD"
+                    descricao = "SSD Kinsgton A400 - SATA"  />
+                    {componenteFeedback}
+                </Cartao>
             <div className = 'col-12 col-lg-6 col-xxl-4 my-2'>
-            <Pedido data="23/04/2023" 
-            icone="fas fa-book fa 2x"
-            titulo = "Livro"
-            descricao = "Concrete Mathematics - Donald Knuth"  />
-            </div>
+                <Cartao cabecalho = "23/04/2023">
+                <Pedido data="23/04/2023" 
+                icone="fas fa-book fa 2x"
+                titulo = "Livro"
+                descricao = "Concrete Mathematics - Donald Knuth"  />
+                {componenteFeedback}
+                </Cartao>
             <div className = 'col-12 col-lg-6 col-xxl-4 my-2'>
-            <Pedido data="26/04/2023" 
-            icone="fas fa-laptop fa 2x"
-            titulo = "Laptop"
-            descricao = "Notebook DELL - 8gb - 256GB"  /> 
+                <Cartao cabecalho = "26/04/2023">
+                <Pedido data="26/04/2023" 
+                icone="fas fa-laptop fa 2x"
+                titulo = "Laptop"
+                descricao = "Notebook DELL - 8gb - 256GB"  /> 
+                {componenteFeedback}
+                </Cartao>
+                </div>
             </div>
         </div>
     </div>
-)
+    </div>
+}
 
 ReactDOM.render(
     <App />, 
--- Aula 09/componentes-e-props/src/pedido.js
+++ Aula 09/componentes-e-props/src/pedido.js
@@ -1,7 +1,5 @@
 const Pedido = (props) => {
-    return <div className="card">
-        <div className="card-header text-muted">{props.data}</div>
-        <div className="card-body d-flex">
+    return <div className="d-flex">
             <div className="d-flex align-items-center">
                 <i className={props.icone}></i>
             </div>
@@ -10,7 +8,6 @@ const Pedido = (props) => {
                 <p className="text-center">{props.descricao}</p>
             </div>
         </div>
-    </div>
 }
 
 export default Pedido;
